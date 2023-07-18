8141e8f06ae99394072420161620968c13b8f7a0
Meefr <mohamedelramah737@gmail.com>
2023-07-18 06:23:51 -0700

    3

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,9 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
- * this function is used for print 10 times
+ * print_alphabet_x10 - prints the alphabet 10 times
+ *
+ * This function takes no parameters and returns no values.
  */
 void print_alphabet_x10(void)
 {
