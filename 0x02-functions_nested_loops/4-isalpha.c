178e8a5b4f2746ff445d660525e1e826b1b2e431
Meefr <mohamedelramah737@gmail.com>
2023-07-18 07:12:37 -0700

    5

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isalpha - function that takes charachter to cheack whether it capital or not.
+ * @c: parameter to check.
+ * Return: 1 if upper or lower and 0 otherwise.
+ */
+int _isalpha(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		if (c >= 65 && c <= 90)
+			return (1);
+		else
+			return (0);
+	}
+	else
+	{
+		return (0);
+	}
+}
