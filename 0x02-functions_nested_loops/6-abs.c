fb0846088385d0bf4dd38be719721049217f5336
Meefr <mohamedelramah737@gmail.com>
2023-07-18 07:53:41 -0700

    6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _abs - calculate the abs of an value.
+ * @x: number to check.
+ * Return: abs of number.
+ */
+int _abs(int x)
+{
+	return (abs(x));
+}
