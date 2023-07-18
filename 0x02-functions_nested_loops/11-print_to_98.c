a061bff38f570683a93a37a65ba746764ef133c0
Meefr <mohamedelramah737@gmail.com>
2023-07-18 15:02:51 -0700

    11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,18 +14,26 @@ void print_to_98(int n)
 			for (i = n; i <= 98; i++)
 			{
 				if (i != 98)
-					printf("%d, ", i);
+				{
+					_putchar(i + '0');
+					_putchar(' ');
+					_putchar(',');
+				}
 				else if (i == 98)
-					printf("%d\n", i);
+					_putchar('\n');
 			}
 		} else if (n >= 98)
 		{
 			for (j = n; j >= 98; j--)
 			{
 				if (j != 98)
-					printf("%d, ", j);
+				{
+					_putchar(j + '0');
+					_putchar(' ');
+					_putchar(',');
+				}
 				else if (j == 98)
-					printf("%d\n", j);
+					_putchar('\n');
 			}
 		}
 }
