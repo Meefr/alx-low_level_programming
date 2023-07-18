623258f95132353b5da8558a160c5ef28f0cf266
Meefr <mohamedelramah737@gmail.com>
2023-07-18 07:58:32 -0700

    9

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print last digit
+ * @x: number to print last digit.
+ * Return: last digit.
+ */
+int print_last_digit(int x)
+{
+	return (x % 10);
+}
