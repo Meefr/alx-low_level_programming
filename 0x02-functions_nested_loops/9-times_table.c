b623c962b114043ebe36ec234cfc5095623f8ec5
Meefr <mohamedelramah737@gmail.com>
2023-07-18 09:30:08 -0700

    9 -3

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,21 +5,35 @@
  */
 void times_table(void)
 {
-	int s = 0;
-	int index;
-	int j;
-	int n = 0;
+int i, j, k;
 
-	for (index = 0; index < 10; index++)
-	{
-		for (j = 0; j < 9; j++)
+
+		for (i = 0; i < 10; i++)
 		{
-			printf(n + ", ");
-			n += s;
+			for (j = 0; j < 10; j++)
+			{
+				k = j * i;
+				if (j == 0)
+				{
+					_putchar(k + '0');
+				}
+
+
+				if (k < 10 && j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(k + '0');
+				} else if (k >= 10)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
+				}
+			}
+			_putchar('\n');
 		}
-		printf(n);
-		s++;
-		n = 0;
-		_putchar('\n');
-	}
+
 }
