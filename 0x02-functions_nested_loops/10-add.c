bd6d63547759286303297d0681e90363d205ce5c
Meefr <mohamedelramah737@gmail.com>
2023-07-18 14:41:46 -0700

    a+b'

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - add x + y
+ * @x: first number
+ * @y: second number
+ * Return: sum of x + y.
+ */
+int add(int x, int y)
+{
+	int sum;
+
+	sum = x + y;
+	return (sum);
+}
