de7166799ece46b6206ade34ae1bae3ea2c20e48
Meefr <mohamedelramah737@gmail.com>
2023-07-18 09:22:58 -0700

    9'

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * times_table - displaay the 9 times table
+ */
+void times_table(void)
+{
+	int s = 1;
+	int index;
+	int j;
+	int n = 0;
+
+	for (index = 0; index < 10; index++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			_putchar(n + '0');
+			n += s;
+		}
+		s++;
+		n = 0;
+	}
+}
